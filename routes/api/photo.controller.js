393c1a9f81277fff07c9e848b145fe4c359e62b3
dmarinsa <dmarinsanchez@gmail.com>
2018-08-02 10:35:51 +0200

    cloudinary

--- routes/api/photo.controller.js
+++ routes/api/photo.controller.js
@@ -2,25 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Recipe = require('../../models/recipe.model');
 const User = require('../../models/user.model');
+const Photo = require('../../models/photo.model')
+const upload = require('../../cloudinary/cloudinary');
 const Clarifai = require('clarifai');
 const clarifaiApp = new Clarifai.App({
  apiKey: process.env.CLARIFAI_KEY
 });
 
-const foodModel = clarifaiApp.models.get('food-items-v1.0')
-
-
-router.get('/', (req, res, next) => {
+router.post('/', upload.single('pic'), (req, res, next) => {
   
-  clarifaiApp.models.predict(Clarifai.FOOD_MODEL, "http://as01.epimg.net/epik/imagenes/2017/10/31/portada/1509469785_213048_1509471547_noticia_normal.jpg")
-  .then( resp => {
-    console.log(resp);
-    res.status(200).json(resp.outputs[0].data)
-  })
-  .catch(err=> {
-    console.log(err);
-    res.status(500).json(err)
-  })
+  const newPhoto = new Photo({
+    filename: req.file.originalname,
+    path: req.file.url
+  });
+
+  newPhoto.save()
+    .then(photo=>{
+      clarifaiApp.models.predict(Clarifai.FOOD_MODEL, photo.path)
+      .then( resp => {
+        console.log(resp);
+        res.status(200).json(resp.outputs[0].data)
+      })
+    })
+    .catch(err=> {
+      console.log(err);
+      res.status(500).json(err)
+    })
 })
 
 module.exports = router;
