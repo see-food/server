ebff0e4995f4b2321ad6a634855672a8d56b376e
jorgeclh <kolmax@gmail.com>
2018-08-05 01:28:19 +0200

    Add starred status to photo schema

--- models/photo.model.js
+++ models/photo.model.js
@@ -8,11 +8,17 @@ const PhotoSchema = Schema({
   },
   filename:{
     type: String,
-    require: true
+    required: true
   },
   recipe: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Recipe'
+    recipeId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Recipe'
+    },
+    starred: {
+      type: Boolean,
+      default: false
+    }
   }],
   user: {
     type: Schema.Types.ObjectId,
@@ -20,13 +26,16 @@ const PhotoSchema = Schema({
   },
   clarifaiInfo:[{
     name: String,
-    value: { 
+    value: {
       type: Number,
       minValue: 0.98
     }
   }]
 }, {
-  timestamps:{ createdAt: 'createdAT', updatedAt:'updatedAt'}
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
 });
 
 const Photo = mongoose.model('Photo', PhotoSchema);
