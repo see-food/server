950aaa814c1374fafa074a0cf033ef071fb9221c
Jorge L <kolmax@gmail.com>
2018-08-08 11:39:04 +0200

    Fix confirmation link on email

--- mailing/send.js
+++ mailing/send.js
@@ -4,18 +4,18 @@ const path = require('path');
 const mjml = require('mjml');
 const hbs = require('handlebars');
 
-const sendMail = (username, email, confirmation) => {
+const sendMail = (username, email, confirmUrl, confirmation) => {
 
   const templateFile = path.join(__dirname, `./templates/confirmation.mjml`);
   const mjmlTemplate = fs.readFileSync(templateFile,'utf8');
   const { html } = mjml(mjmlTemplate, {});
   const templateData = hbs.compile(html);
-  const compiledHTML = templateData({username, confirmation});
+  const compiledHTML = templateData({username, confirmUrl, confirmation});
 
   return transporter.sendMail({
     from: `"SeeFood" <${process.env.EMAIL}>`,
-    to: email, 
-    subject: 'Confirmate your account', 
+    to: email,
+    subject: 'Confirm your account',
     html: compiledHTML
   })
   .then(info => console.log(info))
